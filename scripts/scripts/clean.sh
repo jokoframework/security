5004c6e006738f67cf57ced305ac717c08ea0b13
Dani Cricco <danicricco@sodep.com.py>
2018-03-12 12:02:30 -0300

    Borra archivos innecesarios
    Borra archivos innecesarios del sistema.

--- scripts/scripts/clean.sh
+++
@@ -1,9 +0,0 @@
-# DEMO DB SCRIPT
-TITLE='\033[34m'
-TEXT='\033[33m'
-NC='\033[0m' # No Color
-echo -e " ${TITLE}CLEANING :D${TEXT}"
-
-mvn -s ~/.m2/devtools-settings.xml -Dext.prop.dir=/opt/finhealth/ -Dspring.config.location=file:///opt/finhealth/application.properties -DskipTests -U clean install
-
-echo -e " ${TITLE}FINISHED${NC}"
