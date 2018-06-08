2d3b0a1016ee5909d1a92899d724e828673f6ddf
bsandoval <mbsandoval10@gmail.com>
2018-06-08 14:07:20 -0400

    Elimina imports no utilizados

--- src/test/java/io/github/jokoframework/security/controller/TokenControllerIntegrationTest.java
+++ src/test/java/io/github/jokoframework/security/controller/TokenControllerIntegrationTest.java
@@ -15,7 +15,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -74,8 +73,6 @@ public class TokenControllerIntegrationTest extends AbstractControllerTest {
 	}
 	
 	@Test
-	//@Ignore
-	//FIXME ver issue https://github.com/jokoframework/security/issues/15
 	public void requestingTokenInfoShouldReturnOk() throws Exception {
 		// 1. Creamos el refresh token
     	JokoTokenWrapper token = tokenService.createAndStoreRefreshToken(USER, SECURITY_PROFILE, TOKEN_TYPE.REFRESH,
